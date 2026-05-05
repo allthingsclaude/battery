008b4444e3e7933df71525fc2e28ec09004aa5a4
Marko Radak <mail@markoradak.com>
2026-05-05 22:22:30 +0200

    chore: bump version to 0.3.8

--- Casks/claude-battery.rb
+++ Casks/claude-battery.rb
@@ -1,8 +1,8 @@
 cask "claude-battery" do
-  version "0.3.7"
+  version "0.3.8"
   sha256 "a0d7c41d43167cfea072ed444e0d1cd325c4ccb54489e4339c3241449dd37058"
 
-  url "https://github.com/allthingsclaude/battery/releases/download/v0.3.7/Battery-0.3.7.dmg"
+  url "https://github.com/allthingsclaude/battery/releases/download/v0.3.8/Battery-0.3.8.dmg"
   name "Battery"
   desc "Claude Code usage monitor for your menu bar"
   homepage "https://github.com/allthingsclaude/battery"
